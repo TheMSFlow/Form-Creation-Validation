0b8834ffeadad51e63ed154bed8709e88cc740a6
Michael Steve <themichaelsteve@gmail.com>
2024-07-17 15:43:11 +0100

    updated js file

--- fetch-data.js
+++ fetch-data.js
@@ -1,10 +1,9 @@
-const fetchUserData = async () => {
+async function fetchUserData () {
   const apiUrl = 'https://jsonplaceholder.typicode.com/users';
   const dataContainer = document.getElementById('api-data');
   try {
     const response = await fetch(apiUrl);
     const users = await response.json();
-
     // Apend User List
     dataContainer.innerHTML = '';
     userList = document.createElement('ul');
@@ -14,9 +13,6 @@ const fetchUserData = async () => {
       userList.appendChild(list)
     });
     dataContainer.appendChild(userList);
-    
-  
-
   } catch(error) {
     dataContainer.innerHTML = 'Failed to load use data';
   }
